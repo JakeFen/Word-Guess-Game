a995a833306fed60442c1724d12e9326cdbb1e45
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-07-29 16:09:30 -0400

    added wins, number of guesses, and usersGuesses to show up on the screen when refreshing. Also added Starter image and text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,71 @@
-var wins = 0;
+// gets id wins takes the totalWins count and puts it in place of wins id
+var wins = document.getElementById("wins");
+var totalWins = 0;
+wins.textContent = totalWins;
+
+// gets id guesses-left takes the numberOfGuesses count and puts it in place of guesses-left id
+var guessesLeft = document.getElementById("guesses-left");
 var numberOfGuesses = 6;
+guessesLeft.textContent = numberOfGuesses;
+
+// gets id already-guessed takes the usersGuesses string and puts it in place of alreadyGuessed
+var alreadyGuessed = document.getElementById('already-guessed');
+var usersGuesses = ["_"];
+alreadyGuessed.textContent = usersGuesses;
 
 var overwatchHeros = [
-    "ANA",
-    "ASHE",
-    "BAPTISTE",
-    "BASTION",
-    "BRIGITTE",
-    "DVA",
-    "DOOMFIST",
-    "GENJI",
-    "HANZO",
-    "JUNKRAT",
-    "LUCIO",
-    "MCCREE",
-    "MEI",
-    "MERCY",
-    "MOIRA",
-    "ORISA",
-    "PHARAH",
-    "REAPER",
-    "REINHARDT",
-    "ROADHOG",
-    "SIGMA",
-    "SOLDIER76",
-    "SOMBRA",
-    "SYMMETRA",
-    "TORBJORN",
-    "TACER",
-    "WIDOWMAKER",
-    "WINSTON",
-    "WRECKINGBALL",
-    "ZARYA",
-    "ZENYATTA"
+  "ANA",
+  "ASHE",
+  "BAPTISTE",
+  "BASTION",
+  "BRIGITTE",
+  "DVA",
+  "DOOMFIST",
+  "GENJI",
+  "HANZO",
+  "JUNKRAT",
+  "LUCIO",
+  "MCCREE",
+  "MEI",
+  "MERCY",
+  "MOIRA",
+  "ORISA",
+  "PHARAH",
+  "REAPER",
+  "REINHARDT",
+  "ROADHOG",
+  "SIGMA",
+  "SOLDIER76",
+  "SOMBRA",
+  "SYMMETRA",
+  "TORBJORN",
+  "TACER",
+  "WIDOWMAKER",
+  "WINSTON",
+  "WRECKINGBALL",
+  "ZARYA",
+  "ZENYATTA"
 ];
 
 function Hangman() {
-    // a-z matches lowercase letters
-    // g modifier helps to do match gobally
-    // i case-insensitive
-    var randomHero = overwatchHeros[Math.floor(Math.random()*overwatchHeros.length)];
-    var randomHero = randomHero.replace(/a-z/gi, "_");
+  // a-z matches lowercase letters
+  // g modifier helps to do match gobally
+  // i case-insensitive
+  var randomHero =
+    overwatchHeros[Math.floor(Math.random() * overwatchHeros.length)];
+  var randomHero = randomHero.replace(/a-z/gi, "_");
 
-    // pulls the id hero-name from html and puts randomHero in place of heroName
-    var heroName = document.getElementById('hero-name');
-    heroName.textContent = randomHero;
-
-    console.log(randomHero); 
+  // pulls the id hero-name from html and puts randomHero in place of heroName
+  var heroName = document.getElementById("hero-name");
+  heroName.textContent = randomHero;
 }
 
 Hangman();
 
 function newGame() {
-// when enter is press restart game
-// make function that when enter is pressed run function of hangman
+  // when enter is press restart game
+  // make function that when enter is pressed run function of hangman
 }
-
-// display hero
 
 // when a key is pressed do something
 // add key pressed to list
