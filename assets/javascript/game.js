2affb1cc3d2277c4e28f6ad3ac59ae532bd2e221
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-07-29 14:42:37 -0400

    created some style for hangman game

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+var wins = 0;
+var losses = 0;
+var numberOfGuesses = 6;
+
+var overwatchCharacters = [
+    "ANA",
+    "ASHE",
+    "BAPTISTE",
+    "BASTION",
+    "BRIGITTE",
+    "DVA",
+    "DOOMFIST",
+    "GENJI",
+    "HANZO",
+    "JUNKRAT",
+    "LUCIO",
+    "MCCREE",
+    "MEI",
+    "MERCY",
+    "MOIRA",
+    "ORISA",
+    "PHARAH",
+    "REAPER",
+    "REINHARDT",
+    "ROADHOG",
+    "SIGMA",
+    "SOLDIER76",
+    "SOMBRA",
+    "SYMMETRA",
+    "TORBJORN",
+    "TACER",
+    "WIDOWMAKER",
+    "WINSTON",
+    "WRECKINGBALL",
+    "ZARYA",
+    "ZENYATTA"
+];
+
+// Pull random hero from array
+// display hero
+// when a key is pressed do something
