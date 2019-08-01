e567394adb509425a7035858155ef72ceb9786c5
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-08-01 11:32:37 -0400

    allows you to only guess the same letter 1x, only lets you guess letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,11 +47,38 @@ var overwatchHeros = [
 var rightLetter = [];
 var wrongLetter = [];
 var underScore = [];
+var letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
 
 function Hangman() {
   var randomHero =
     overwatchHeros[Math.floor(Math.random() * overwatchHeros.length)];
-
   // adds underscore to UnderScore for ever letter thats in randomHero
   for (var i = 0; i < randomHero.length; i++) {
     underScore.push("_");
@@ -59,52 +86,48 @@ function Hangman() {
 
   // pulls the id hero-name from html and puts underScore in place of heroName
   var heroName = document.getElementById("hero-name");
-  heroName.textContent = underScore;
+  heroName.textContent = underScore.join(" ");
 
   // Finding what key was pressed and printing it to screen
   document.onkeyup = function(event) {
     var usersGuess = event.key.toUpperCase();
-    if (randomHero.indexOf(usersGuess) > -1) {
-      // add to write letter array
-      rightLetter.push(usersGuess);
-      underScore[randomHero.indexOf(usersGuess)] = usersGuess;
-      heroName.textContent = underScore;
-      if (underScore.join("") === randomHero) {
-        youWon();
+    if (letters.includes(usersGuess)) {
+      if (randomHero.includes(usersGuess)) {
+        // add to rightletter array
+        rightLetter.push(usersGuess);
+        underScore[randomHero.indexOf(usersGuess)] = usersGuess;
+        heroName.textContent = underScore.join(" ");
+
+        if (underScore.join("") === randomHero) {
+          youWon();
+        }
+      } else if(wrongLetter.includes(usersGuess)) {
+        alert("You have already guessed this Letter!");
       }
-    } else {
-      wrongLetter.push(usersGuess);
-      numberOfGuesses--;
-      guessesLeft.textContent = numberOfGuesses;
-      alreadyGuessed.textContent = wrongLetter;
-      if (numberOfGuesses === 0) {
-        alert("You have lost!");
+      else {
+        wrongLetter.push(usersGuess);
+        numberOfGuesses--;
+        guessesLeft.textContent = numberOfGuesses;
+        alreadyGuessed.textContent = wrongLetter;
+        if (numberOfGuesses === 0) {
+          alert("You have lost!");
+          reset();
+        }
       }
     }
   };
 
   function youWon() {
-    alert("You Won!");
+    alert("You Won! Press the 'Enter' key to start a new game!");
     totalWins++;
     wins.textContent = totalWins;
   }
-
-  // document.onkeyup = function(event) {
-  //   var usersGuess = event.key;
-  //   if (randomHero.indexOf(usersGuess) > -1) {
-  //     rightLetter.push(usersGuess);
-  //     console.log(randomHero);
-  //   }
-  // }
-
-  //when enter is press restart game
-  //make function that when enter is pressed run function of hangman
 }
 
 Hangman();
 
-// lower number of guesses when pressing wrong key
-
-// add win when they guess the word correct
-
 // change image when word is revealed
+
+// need to figure out how to find all of the same letters in the array
+// and pop up on the screen
+
